2a9d1c3ce6dc39b1d24647327fea9c476ca5d576
Juanan <juan-antonio.sanchez@necomplus.com>
2023-12-15 23:54:54 +0100

    🔧 Quasar sass migrator in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,41 @@
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import AutoImport from 'unplugin-auto-import/vite';
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import { sassMigratorQuasar } from 'rollup-plugin-sass-migrator';
 import path from 'node:path';
 
 export default defineConfig({
-    resolve: {
-      alias: {
-        src: path.resolve(__dirname, 'src'),
-      },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
     },
-    plugins: [
-        Vue({
-            template: { transformAssetUrls }
-        }),
+  },
+  plugins: [
+    Vue({
+      template: { transformAssetUrls },
+    }),
 
-        quasar({
-            autoImportComponentCase: 'pascal',
-            sassVariables: 'src/quasar-variables.sass'
-        }),
+    quasar({
+      autoImportComponentCase: 'pascal',
+      sassVariables: 'src/quasar-variables.sass',
+    }),
 
-        AutoImport({
-          imports: [
-            'vue',
-          ],
-          dts: 'src/auto-imports.d.ts',
-          vueTemplate: true,
-        }),
+    AutoImport({
+      imports: [
+        'vue',
+      ],
+      dts: 'src/auto-imports.d.ts',
+      vueTemplate: true,
+    }),
 
-        Components({
-          extensions: ['vue', 'md'],
-          include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-          dts: 'src/components.d.ts',
-        }),
-    ]
-})
+    Components({
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'src/components.d.ts',
+    }),
+
+    sassMigratorQuasar(),
+  ],
+});
